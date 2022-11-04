305e118693c0bcf4ba0faa25f892c9e8b17852f7
Yony Calsin <helloyonycalsin@gmail.com>
2022-11-03 19:13:34 -0500

    fix: styles

--- app/core/components/LabeledTextField.tsx
+++ app/core/components/LabeledTextField.tsx
@@ -19,7 +19,8 @@ export const LabeledTextField = forwardRef<HTMLInputElement, LabeledTextFieldPro
       formState: { isSubmitting, errors },
     } = useFormContext()
     const error = Array.isArray(errors[name])
-      ? errors[name].join(", ")
+      ? // @ts-expect-error ts(2339)
+        errors[name]?.join(", ")
       : errors[name]?.message || errors[name]
 
     return (
